671dd47a269e373bde044749b1715897587d3b4c
Lewis Benson <tm.lewis.benson@gmail.com>
2024-02-22 20:13:29 -0500

    add dom feature to greet

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 'use strict';
 function helloWorld(person) {
+  const body = document.querySelector('body');
+  const p = document.createElement('p');
+  p.innerText = 'Hello ' + person;
+  body.appendChild(p);
   console.log('Hello ' + person);
 }
 
