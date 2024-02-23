54c02336e5195f2b7957bdf38135c5c609b799ad
Lewis Benson <tm.lewis.benson@gmail.com>
2024-02-22 20:46:15 -0500

    add more functions

--- app.js
+++ app.js
@@ -4,7 +4,25 @@ function helloWorld(person) {
   const p = document.createElement('p');
   p.innerText = 'Hello ' + person;
   body.appendChild(p);
-
+}
+function helloWorl2d(person) {
+  const body = document.querySelector('body');
+  const p = document.createElement('p');
+  p.innerText = 'Hello ' + person;
+  body.appendChild(p);
+}
+function helloWor4ld(person) {
+  const body = document.querySelector('body');
+  const p = document.createElement('p');
+  p.innerText = 'Hello ' + person;
+  body.appendChild(p);
+}
+function helloWorld2(person) {
+  const body = document.querySelector('body');
+  const p = document.createElement('p');
+  p.innerText = 'Hello ' + person;
+  body.appendChild(p);
+  console.log('Hello ' + person, body, p);
 }
 
 helloWorld('Bob');
